85564de33dd62edd126e3e66deb1517679635ce8
markmdoza <marklmdoza@gmail.com>
2024-05-28 13:22:46 -0400

    rework file structure

---
+++ 001/day01-variables.py
@@ -0,0 +1,9 @@
+# name = input("What is your name?")
+# print(name)
+
+# print(len(input()))
+name = input('What is your name?')
+length = len(name)
+print(length)
+# create a third variable to switch two variable inputs.
+# variables with more than one word need to be separated by an underscore.
