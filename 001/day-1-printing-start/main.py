70c6a4e41e065dd7686ae4638dddc6db0a9b5d5a
markmdoza <marklmdoza@gmail.com>
2024-05-28 12:04:11 -0400

    day 01 printing complete

--- 001/day-1-printing-start/main.py
+++ 001/day-1-printing-start/main.py
@@ -1,2 +1,4 @@
 # Write your code below this line 👇
-print('Hello World!')
+print('Hello World!') # single line
+print('Hello world!\nHello world again!') # \n creates a new line.
+print('Hello ' + 'Mark') # spaces must be present within string to create a space inside the console. Or an actual ' '.
