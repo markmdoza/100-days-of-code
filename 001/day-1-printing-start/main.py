d1ac3f45dc5b2dcff0e6a143e26c5cefbf9f87f5
markmdoza <marklmdoza@gmail.com>
2024-05-28 12:19:12 -0400

    day 01 input function complete

--- 001/day-1-printing-start/main.py
+++ 001/day-1-printing-start/main.py
@@ -1,4 +1,12 @@
 # Write your code below this line 👇
-print('Hello World!') # single line
-print('Hello world!\nHello world again!') # \n creates a new line.
-print('Hello ' + 'Mark') # spaces must be present within string to create a space inside the console. Or an actual ' '.
+# print('Hello World!') # single line
+# print('Hello world!\nHello world again!') # \n creates a new line.
+# print('Hello ' + 'Mark') # spaces must be present within string to create a space inside the console.
+# Or an actual ' '.
+
+# The Python Input Function
+# input() will get user input in console
+# then prin() will print the word 'hello' and the user input
+# print("Hello " + input('What is your name?'))
+# input gets executed and prompts the user. The input from the user replaces the actual input code and then the string
+# concatenates with the input and executes the new string.
