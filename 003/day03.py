286bc5180f63c5a135e4458ef0062ff2a2541211
markmdoza <marklmdoza@gmail.com>
2024-05-31 13:27:53 -0400

    day 03 start

---
+++ 003/day03.py
@@ -0,0 +1,31 @@
+# If, else statements. Here we go again!
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you need to be taller to ride the rollercoaster!")
+
+# Odd or Even Challenge
+# Which number do you want to check?
+# number = int(input())
+# # 🚨 Don't change the code above 👆
+#
+# # Write your code below this line 👇
+#
+# if number % 2:
+#   print("This is an odd number.")
+# else:
+#   print("This is an even number.")
+
+# Angela's solution
+# Which number do you want to check?
+# number = int(input())
+
+# If the number can be divided by 2 with 0 remainder.
+# if number % 2 == 0:
+#   print("This is an even number.")
+# Otherwise (number cannot be divided by 2 with 0 remainder).
+# else:
+#   print("This is an odd number.")
